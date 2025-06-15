aff3bb8c278dfa5a3f0d647d743233da14470142
Pat <pat@MacBook-Air-de-Pat.local>
2025-06-15 15:44:34 -0600

    Primera versión

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+function mostrarPopup(btn) {
+    const popup = btn.nextElementSibling;
+    popup.style.display = "flex";
+}
+
+function cerrarPopup(span) {
+    const popup = span.closest(".popup-info");
+    popup.style.display = "none";
+  }
